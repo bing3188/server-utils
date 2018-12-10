610de757ce9787c1bd6e9b8e6229cd9cf15c657e
huliqun <huliquns@126.com>
2018-12-10 14:50:02 +0800

    封装authority 以及redisClient

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const rabbitmqClinet = require('./lib/rabbitmqClinet')
 const websocketUtil = require('./lib/websocketUtil')
+const redisClient = require('./lib/redisClient')
+const authority = require('./lib/authority')
 
 const setLogger = appointLogger => {
     rabbitmqClinet.setLogger(appointLogger)
+    redisClient.setLogger(appointLogger)
     websocketUtil.setLogger(appointLogger)
+    authority.setLogger(appointLogger)
 }
 
 module.exports = {
   setLogger: setLogger,
+  authority: authority,
   rabbitmqClinet: rabbitmqClinet,
+  redisClient: redisClient,
   websocketUtil: websocketUtil
 }
