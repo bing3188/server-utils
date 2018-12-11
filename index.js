4c07def0c825ce2e3921ff0b38f27528934b4c86
huliqun <huliquns@126.com>
2018-12-11 14:24:15 +0800

    增加 mongoClient elasticsearchClient mysqlClient

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const websocketUtil = require('./lib/websocketUtil')
 const redisClient = require('./lib/redisClient')
 const authority = require('./lib/authority')
 const scheduleJob = require('./lib/scheduleJob')
+const mongoClient = require('./lib/mongoClient')
+const elasticsearchClient = require('./lib/elasticsearchClient')
+const mysqlClient = require('./lib/mysqlClient')
 
 const setLogger = appointLogger => {
-    rabbitmqClinet.setLogger(appointLogger)
-    redisClient.setLogger(appointLogger)
-    websocketUtil.setLogger(appointLogger)
-    authority.setLogger(appointLogger)
-    scheduleJob.setLogger(appointLogger)
+  rabbitmqClinet.setLogger(appointLogger)
+  redisClient.setLogger(appointLogger)
+  websocketUtil.setLogger(appointLogger)
+  authority.setLogger(appointLogger)
+  scheduleJob.setLogger(appointLogger)
+  mongoClient.setLogger(appointLogger)
+  mysqlClient.setLogger(appointLogger)
 }
 
 module.exports = {
@@ -18,5 +23,8 @@ module.exports = {
   rabbitmqClinet: rabbitmqClinet,
   redisClient: redisClient,
   scheduleJob: scheduleJob,
-  websocketUtil: websocketUtil
+  websocketUtil: websocketUtil,
+  mongoClient: mongoClient,
+  elasticsearchClient: elasticsearchClient,
+  mysqlClient: mysqlClient
 }
