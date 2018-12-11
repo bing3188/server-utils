96b0b16410555ac52b4d14ee0069fcefc34591f2
huliqun <huliquns@126.com>
2018-12-11 14:43:09 +0800

    回滚修改

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const websocketUtil = require('./lib/websocketUtil')
 const redisClient = require('./lib/redisClient')
 const authority = require('./lib/authority')
 const scheduleJob = require('./lib/scheduleJob')
-const mongoClient = require('./lib/mongoClient')
-const elasticsearchClient = require('./lib/elasticsearchClient')
-const mysqlClient = require('./lib/mysqlClient')
 
 const setLogger = appointLogger => {
-  rabbitmqClinet.setLogger(appointLogger)
-  redisClient.setLogger(appointLogger)
-  websocketUtil.setLogger(appointLogger)
-  authority.setLogger(appointLogger)
-  scheduleJob.setLogger(appointLogger)
-  mysqlClient.setLogger(appointLogger)
+    rabbitmqClinet.setLogger(appointLogger)
+    redisClient.setLogger(appointLogger)
+    websocketUtil.setLogger(appointLogger)
+    authority.setLogger(appointLogger)
+    scheduleJob.setLogger(appointLogger)
 }
 
 module.exports = {
@@ -22,8 +18,5 @@ module.exports = {
   rabbitmqClinet: rabbitmqClinet,
   redisClient: redisClient,
   scheduleJob: scheduleJob,
-  websocketUtil: websocketUtil,
-  mongoClient: mongoClient,
-  elasticsearchClient: elasticsearchClient,
-  mysqlClient: mysqlClient
+  websocketUtil: websocketUtil
 }
